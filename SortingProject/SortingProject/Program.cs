95be090970da43a1a160bcf40368fed5582fd7f4
myunglyu <180991957+myunglyu@users.noreply.github.com>
2024-11-13 09:55:00 -0800

    Add project files.

---
+++ SortingProject/SortingProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.Design;
+
+namespace SortingProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 1, 5, 4, 2, 34, 67, 86, 85, 34, 23, 64, 73, 76 };
+            Program.Print(arr);
+            int[] a = { 1, 3, 5, 7, 12, 13 };
+            int[] b = { 2, 4, 6, 8, 34, 35 };
+            int[] c = new int[a.Length + b.Length];
+            Sort.Merge(a, b, c);
+            Print(c);
+            Sort.MergeSort(arr);
+            Print(arr);
+
+        }
+        public static void Print(int[] arr)
+        {
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SortingProject/SortingProject/Sort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingProject
+{
+    public class Sort
+    {
+        public static void MergeSort(int[] arr)
+        {
+            if (arr.Length > 1)
+            {
+            int[] a = new int[arr.Length / 2];
+            int[] b = new int[arr.Length - arr.Length / 2];
+
+            for (int i = 0; i < a.Length; i++) { a[i] = arr[i]; }
+            for (int i = 0; i < b.Length; i++) { b[i] = arr[i + a.Length]; }
+
+                Program.Print(a);
+                Program.Print(b);
+
+            MergeSort(a);
+            MergeSort(b);
+            Merge(a, b, arr);
+            }
+        }
+        public static void Merge(int[] a, int[] b, int[] c)
+        {
+            int i = 0, j = 0, k = 0;
+
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j]) { c[k++] = a[i++]; }
+                else { c[k++] = b[j++]; }
+            }
+            while (i < a.Length) { c[k++] = a[i++]; }
+            while (j < b.Length) { c[k++] = b[j++]; }
+
+        }
+
+
+    }
+}
